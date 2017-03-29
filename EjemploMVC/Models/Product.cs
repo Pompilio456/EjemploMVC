ad3cd144310e31b6aa1ed5b8da576a5a2bd28bf2
yilber.calderon <yilberandrey@gmail.com>
2017-03-29 15:33:20 -0500

    Se creo la clase Product , con sus propiedades y se creo su controlador

---
+++ EjemploMVC/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EjemploMVC.Controllers
+{
+    public class Product
+    {
+        [Key] //data notation key, que nos maneja el ID
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Lastbuy { get; set; }
+        public float Stock { get; set; }
+
+    }
+}
